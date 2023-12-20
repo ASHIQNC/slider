75aec43f4ef13842e0a53fb4a4415f0275d246e2
ASHIQNC <ashiqrahman55@gmail.com>
2023-12-21 01:18:11 +0530

    removed unwanted commented code and images

--- src/imageSlider/ImageSlider.js
+++ src/imageSlider/ImageSlider.js
@@ -1,65 +1,3 @@
-// import React, { useRef } from "react";
-// import moon from "../assets/moon.png";
-// import land from "../assets/land.png";
-// import cat from "../assets/cat.gif";
-
-// import { Parallax, ParallaxLayer } from "@react-spring/parallax";
-
-// const ImageSlider = () => {
-//   // by using useRef we can jumb in to that location while clicking on the text
-//   const ref = useRef();
-//   return (
-//     <div>
-//       <Parallax style={{ backgroundColor: "black" }} pages={4} ref={ref}>
-//         {/* by default factor will be one which means its size is equal to one page
-//         but we can make to half of the page or more than one page by updating the factor  */}
-//         {/* The offset of the layer when it's corresponding page is fully in view  */}
-//         <ParallaxLayer
-//           offset={0}
-//           speed={1}
-//           factor={2}
-//           style={{
-//             backgroundImage: `url(${moon})`,
-//             backgroundSize: "cover",
-//           }}
-//         />
-
-//         <ParallaxLayer
-//           offset={2}
-//           speed={1}
-//           factor={4}
-//           style={{
-//             backgroundImage: `url(${land})`,
-//             backgroundSize: "cover",
-//           }}></ParallaxLayer>
-
-//         {/* here the giff we start at begining of second page and ends at half of third page */}
-//         <ParallaxLayer
-//           sticky={{ start: 0.9, end: 2.5 }}
-//           style={{ textAlign: "center" }}>
-//           <img src={cat} />
-//         </ParallaxLayer>
-
-//         <ParallaxLayer
-//           offset={0.2}
-//           speed={0.05}
-//           onClick={() => ref.current.scrollTo(3)}>
-//           <h2>Welcome to my website</h2>
-//         </ParallaxLayer>
-
-//         <ParallaxLayer
-//           offset={3}
-//           speed={2}
-//           onClick={() => ref.current.scrollTo(0)}>
-//           <h2>Hi Mom!</h2>
-//         </ParallaxLayer>
-//       </Parallax>
-//     </div>
-//   );
-// };
-
-// export default ImageSlider;
-
 import React, { useRef, useState } from "react";
 import land from "../assets/land.png";
 import cat from "../assets/cat.gif";
@@ -77,12 +15,10 @@ const ImageSlider = () => {
     require("../assets/pic3.jpg"),
     require("../assets/pic4.jpg"),
     require("../assets/moon.png"),
-    // Add more images as needed
   ];
   const parallaxRef = useRef();
   const [sliderIndex, setSliderIndex] = useState(0);
-
-  console.log("speed", speed);
+  const [speed, setSpeed] = useState(1);
 
   const handlePrevClick = () => {
     setSliderIndex(
@@ -108,13 +44,13 @@ const ImageSlider = () => {
       {/* The offset of the layer when it's corresponding page is fully in view  */}
       <Parallax
         style={{ backgroundColor: "black" }}
-        pages={3}
+        pages={4}
         ref={parallaxRef}>
         {images.map((image, index) => (
           <ParallaxLayer
             key={index}
             offset={index}
-            speed={1}
+            speed={speed}
             style={{ height: "100%" }}>
             <animated.img style={slide} src={images[sliderIndex]} alt="image" />
           </ParallaxLayer>
@@ -129,7 +65,7 @@ const ImageSlider = () => {
           }}></ParallaxLayer>
         {/* here the giff we start at begining of second page and ends at half of third page */}
         <ParallaxLayer
-          sticky={{ start: 0.3, end: 2.5 }}
+          sticky={{ start: 0.3, end: 2.9 }}
           style={{ textAlign: "center" }}>
           <img src={cat} />
         </ParallaxLayer>
